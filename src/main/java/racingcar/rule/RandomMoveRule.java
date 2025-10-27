7910fd57115cde462f250df5c9946ee51094e829
sanghuniolsida <p0526000@gmail.com>
2025-10-27 15:25:20 +0900

    refactor(rule): RandomMoveRule 변수명 개선 (의미 명확화)

--- src/main/java/racingcar/rule/RandomMoveRule.java
+++ src/main/java/racingcar/rule/RandomMoveRule.java
@@ -3,18 +3,18 @@ package racingcar.rule;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public final class RandomMoveRule {
-    private static final int MIN = 0;
-    private static final int MAX = 9;
-    private static final int THRESHOLD = 4;
+    private static final int RANDOM_MIN_INCLUSIVE = 0;
+    private static final int RANDOM_MAX_INCLUSIVE = 9;
+    private static final int MOVE_THRESHOLD = 4;
 
     private RandomMoveRule() { }
 
-    public static boolean isMovable(int value) {
-        return value >= THRESHOLD;
+    public static boolean isMovable(int randomValue) {
+        return randomValue >= MOVE_THRESHOLD;
     }
 
     public static boolean shouldMove() {
-        int value = Randoms.pickNumberInRange(MIN, MAX);
-        return isMovable(value);
+        int randomValue = Randoms.pickNumberInRange(RANDOM_MIN_INCLUSIVE, RANDOM_MAX_INCLUSIVE);
+        return isMovable(randomValue);
     }
 }
