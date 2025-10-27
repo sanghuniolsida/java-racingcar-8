ad7eb193fc5388a4f93b4a53adb19d22f34a2b37
sanghuniolsida <p0526000@gmail.com>
2025-10-27 15:52:53 +0900

    feat(app): Application main 연결 및 예외 메시지 출력

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,19 @@
 package racingcar;
 
+import camp.nextstep.edu.missionutils.Console;
+import racingcar.controller.GameController;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            GameController controller = new GameController(new InputView(), new OutputView());
+            controller.startGame();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        } finally {
+            Console.close();
+        }
     }
 }
